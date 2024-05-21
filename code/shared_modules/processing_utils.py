9ffa72b8a1d9c896cae599e352ed5dd27a815391
Xxxyyyzin <77394937+Xxxyyyzin@users.noreply.github.com>
2024-05-21 18:30:23 +0800

    Update processing_utils.py

--- code/shared_modules/processing_utils.py
+++ code/shared_modules/processing_utils.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 12 16:57:30 2019
-
-@author: bmoseley
-"""
-
+# DISCLAIMER: This script is borrowed from Moseley (2018-2020).
+# Source: [https://github.com/benmoseley/seismic-simulation-complex-media]
 
 # This module defines two functions for converting between 1D profiles of horizontally layered
 # velocity models and their corresponding normal incidence reflectivity series. 
